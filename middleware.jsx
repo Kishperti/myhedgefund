06aa8b1df8bce05bc32e2bb06662d4ae598fcf32
Sahil Malik <sahilmalik25007@gmail.com>
2024-06-19 20:28:20 +0530

    Delete middleware.jsx

--- middleware.jsx
+++
@@ -1,9 +0,0 @@
-
-import { NextResponse } from 'next/server';
-
-export function middleware(request) {
-  // if(request.nextUrl.pathname!="/maintenance"){
-  //   return NextResponse.redirect(new URL('/maintenance', request.url));
-
-  // }
-}
